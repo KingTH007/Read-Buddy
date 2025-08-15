fc514510864e92e067969325568b2ef57cd83517
KingTH007 <kingthytchannels@gmail.com>
2025-08-16 04:06:33 +0800

    added learn-act for student lessons

---
+++ Back-end/js/stud-func.js
@@ -0,0 +1,4 @@
+document.getElementById("learn1").addEventListener("click", function () {
+        // Change "lesson.html" to your actual lesson page
+        window.location.href = "../../Front-end/html/learn-act.html";
+    });
